809ff176cc47933506a20c9d6555dbde6a42a86d
iida <iida@ucl.nuee.nagoya-u.ac.jp>
2018-08-14 10:57:14 +0900

    remove unused cords

--- lambda_function.py
+++ lambda_function.py
@@ -1,11 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-This sample demonstrates a simple skill built with the Amazon Alexa Skills Kit.
-The Intent Schema, Custom Slots, and Sample Utterances for this skill, as well
-as testing instructions are located at http://amzn.to/1LzFrj6
-
-For additional samples, visit the Alexa Skills Kit Getting Started guide at
-http://amzn.to/1LGWsLG
+generate random number
 """
 
 from __future__ import print_function
@@ -51,83 +46,55 @@ def get_welcome_response():
     """
 
     session_attributes = {}
-    card_title = "Welcome"
-    speech_output = "Welcome to the Alexa Skills Kit sample. " \
-                    "Please tell me your favorite color by saying, " \
-                    "my favorite color is red"
+    card_title = "乱数ジェネレータ"
+    speech_output = "あなたのほしい乱数を生成します。" \
+                    "乱数を生成などと言ってみてください"
+
     # If the user either does not reply to the welcome message or says something
     # that is not understood, they will be prompted again with this text.
-    reprompt_text = "Please tell me your favorite color by saying, " \
-                    "my favorite color is red."
+    reprompt_text = "あなたのほしい乱数を生成します。" \
+                    "乱数を生成などと言ってみてください"
+
     should_end_session = False
     return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, reprompt_text, should_end_session))
 
 
 def handle_session_end_request():
-    card_title = "Session Ended"
-    speech_output = "Thank you for trying the Alexa Skills Kit sample. " \
-                    "Have a nice day! "
+    # card_title = "Session Ended"
+    card_title = "セッション終了"
+
+    speech_output = "さようなら。 "
+
     # Setting this to true ends the session and exits the skill.
     should_end_session = True
     return build_response({}, build_speechlet_response(
         card_title, speech_output, None, should_end_session))
 
 
-def create_favorite_color_attributes(favorite_color):
-    return {"favoriteColor": favorite_color}
-
-
-def set_color_in_session(intent, session):
-    """ Sets the color in the session and prepares the speech to reply to the
-    user.
+def generate_random_num(intent, session, num1=1, num2=6):
+    """ Create random number
     """
-
-    card_title = intent['name']
     session_attributes = {}
     should_end_session = False
 
-    if 'Color' in intent['slots']:
-        favorite_color = intent['slots']['Color']['value']
-        session_attributes = create_favorite_color_attributes(favorite_color)
-        speech_output = "I now know your favorite color is " + \
-                        favorite_color + \
-                        ". You can ask me your favorite color by saying, " \
-                        "what's my favorite color?"
-        reprompt_text = "You can ask me your favorite color by saying, " \
-                        "what's my favorite color?"
-    else:
-        speech_output = "I'm not sure what your favorite color is. " \
-                        "Please try again."
-        reprompt_text = "I'm not sure what your favorite color is. " \
-                        "You can tell me your favorite color by saying, " \
-                        "my favorite color is red."
+    if 'slots' in intent:
+        num1 = int(intent['slots']['firstNum'].get('value', num1))
+        num2 = int(intent['slots']['secondNum'].get('value', num2))
+
+    # card_title = intent['name']
+    card_title = "{}から{}の乱数".format(num1, num2)
+    print("create random number from {} to {}".format(num1, num2))
+    
+    num = random.randint(num1, num2)
+    speech_output = str(num) + "です"
+
+    reprompt_text = speech_output
     return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, reprompt_text, should_end_session))
 
-
-def get_color_from_session(intent, session):
-    session_attributes = {}
-    reprompt_text = None
-
-    if session.get('attributes', {}) and "favoriteColor" in session.get('attributes', {}):
-        favorite_color = session['attributes']['favoriteColor']
-        speech_output = "Your favorite color is " + favorite_color + \
-                        ". Goodbye."
-        should_end_session = True
-    else:
-        speech_output = "I'm not sure what your favorite color is. " \
-                        "You can say, my favorite color is red."
-        should_end_session = False
-
-    # Setting reprompt_text to None signifies that we do not want to reprompt
-    # the user. If the user does not respond or says something that is not
-    # understood, the session will end.
-    return build_response(session_attributes, build_speechlet_response(
-        intent['name'], speech_output, reprompt_text, should_end_session))
-
-
 # --------------- Events ------------------
+
 
 def on_session_started(session_started_request, session):
     """ Called when the session starts """
@@ -161,10 +128,6 @@ def on_intent(intent_request, session):
         return generate_random_num(intent, session)
     elif intent_name == "RangeRandNumIntent":
         return generate_random_num(intent, session)
-    elif intent_name == "MyColorIsIntent":
-        return set_color_in_session(intent, session)
-    elif intent_name == "WhatsMyColorIntent":
-        return get_color_from_session(intent, session)
     elif intent_name == "AMAZON.HelpIntent":
         return get_welcome_response()
     elif intent_name == "AMAZON.CancelIntent" or intent_name == "AMAZON.StopIntent":
@@ -182,27 +145,6 @@ def on_session_ended(session_ended_request, session):
           ", sessionId=" + session['sessionId'])
     # add cleanup logic here
 
-
-def generate_random_num(intent, session, num1=1, num2=6):
-    """ Create random number
-    """
-    card_title = intent['name']
-    session_attributes = {}
-    should_end_session = False
-
-    if 'slots' in intent:
-        num1 = int(intent['slots']['firstNum'].get('value', num1))
-        num2 = int(intent['slots']['secondNum'].get('value', num2))
-
-    print("create random number from {} to {}".format(num1, num2))
-    
-    num = random.randint(num1, num2)
-    
-    speech_output = str(num) + 'です'
-
-    reprompt_text = speech_output
-    return build_response(session_attributes, build_speechlet_response(
-        card_title, speech_output, reprompt_text, should_end_session))
 
 # --------------- Main handler ------------------
 
